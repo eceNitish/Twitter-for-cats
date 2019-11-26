6b65d97c7bd3324c022e5068094643dd4ef79a65
eceNitish <nitish6776@hotmail.com>
2019-11-26 15:43:02 +0530

    second commit

--- client/client.js
+++ client/client.js
@@ -2,10 +2,13 @@ console.log("Hello World")
 
 const form = document.querySelector('form');
 const loadingElement = document.querySelector('.loading');
+const mewsElement = document.querySelector('.mews');
 const API_URL = "http://localhost:3000/mews";
 
-loadingElement.style.display = 'none';
+loadingElement.style.display = '';
 //console.log(form.name);
+
+listAllMews();
 
 form.addEventListener("submit", (event) => {
     event.preventDefault();
@@ -33,7 +36,32 @@ form.addEventListener("submit", (event) => {
         body : JSON.stringify(mew)
     }).then(res => res.json())
         .then(createdMew => {
-            console.log(createdMew);
+            //console.log(createdMew);
+            loadingElement.style.display = 'none';
+            form.reset();
+            form.style.display = '';
         });
 
 })
+
+function listAllMews () {
+    fetch(API_URL)
+        .then(res => res.json())
+        .then(mews => {
+            console.log(mews);
+            mews.forEach(mew => {
+                const div = document.createElement('div');
+
+                const header = document.createElement('h3');
+                header.textContent = mew.name;
+
+                const content = document.createElement('p');
+                content.textContent = mew.content;
+
+                div.appendChild(header);
+                div.appendChild(content);
+
+                mewsElement.appendChild(div);
+            });      
+        })
+};
